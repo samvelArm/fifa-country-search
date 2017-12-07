c252c23fd9e632317ccb0899f98318511472402a
Samvel Antanyan <samvelantanyan@workfront.com>
2017-12-07 16:56:39 +0400

    added redux to enter file.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/Main.jsx';
+import Main from './components/Main';
+import { Provider } from 'react-redux'
+
+import store from './redux/store'
 
 var root = document.getElementById('mount');
 
-ReactDOM.render(<Main/>, root);
+ReactDOM.render(<Provider store={store}><Main/></Provider>, root);
